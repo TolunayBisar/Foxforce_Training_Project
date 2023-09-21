cc200bc91c9e46b2e42a7bc2af46d6d633529370
tolunaybisar <tolunay.bisar@gmail.com>
2023-09-21 18:07:26 +0300

    my project 2

--- src/test/java/order/OrderPage.java
+++ src/test/java/order/OrderPage.java
@@ -5,24 +5,89 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
 public class OrderPage {
     WebDriver driver;
     FunctionLibrary functionLibrary;
-    @FindAll(@FindBy(xpath="//div/table[last()]/tbody/tr") )
+    @FindAll(@FindBy(xpath = "//div/table[last()]/tbody/tr"))
     List<WebElement> orderList;
+
+    @FindBy(linkText = "Check/Uncheck all")
+    WebElement checkUncheckallClick;
+
+    @FindAll(@FindBy(xpath = "//div[@class='custom-checkbox']"))
+    List<WebElement> checkboxList;
+    @FindAll(@FindBy(xpath = "//div[@class='custom-checkbox selected']"))
+    List<WebElement> checkboxSelectedList;
+
+    @FindBy(xpath = "//div[@class='tab']/a[text()='Create Order']")
+    WebElement createOrderTab;
+
+    @FindBy(id = "o_status")
+    WebElement orderStatusField;
+
+    @FindBy(xpath = "//div[@id='tab_order_billing']/a[text()='Billing']")
+    WebElement billingTab;
+
+    @FindBy(id="sum_name")
+    WebElement customerInfoField;
+
+    public OrderPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        functionLibrary = new FunctionLibrary(driver);
+    }
 
 
     // Verify at least one order in list
-    public boolean verifyOrder(){
-        if (orderList.size()>1)
+    public boolean verifyOrder() {
+        if (orderList.size() >= 1)
             System.out.println("There is at leat on order in list");
-        System.out.printf("There are %d orders in this list",orderList.size());
+        System.out.printf("There are %d orders in this list", orderList.size());
 
 
         return true;
     }
 
+    //'Check/uncheck all' click
+    public boolean verifyCheckUncheckAllClick() {
+        functionLibrary.waitForElementPresent(checkUncheckallClick);
+        checkUncheckallClick.click();
+        functionLibrary.sleep(1);
+        checkUncheckallClick.click();
+        functionLibrary.sleep(1);
+
+        System.out.println(checkboxList.size());
+        System.out.println(orderList.size());
+        System.out.println(checkboxSelectedList.size());
+
+        if ((checkboxSelectedList.size() == orderList.size()) || (checkboxList.size() == orderList.size()))
+            System.out.println("Check is working");
+
+
+        return true;
+    }
+
+    //Create order
+    public void createOrder() {
+        functionLibrary.waitForElementPresent(createOrderTab);
+        createOrderTab.click();
+        functionLibrary.waitForElementPresent(orderStatusField);
+        orderStatusField.click();
+        Select statusDropdown = new Select(orderStatusField);
+        for (int i = 1; i <= 6; i++) {
+            statusDropdown.selectByIndex(i);
+         functionLibrary.waitForElementPresent(billingTab);
+         billingTab.click();
+
+        }
+
+    }
 }
+
+
+
--- src/test/java/order/RunOrder.java
+++ src/test/java/order/RunOrder.java
@@ -4,32 +4,50 @@ import basefunctions.BaseClass;
 import dashboard.DashBoardPage;
 import dashboard.LoginPage;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class RunOrder extends BaseClass {
     LoginPage loginPage;
+    DashBoardPage dashBoardPage;
+    OrderPage orderPage;
 
 
     @BeforeClass
     public void setUp() {
         openBrowser("http://cubecartuat.unitedcoder.com/admin_tu8sml.php");
         loginPage = new LoginPage(driver);
+        dashBoardPage = new DashBoardPage(driver);
+        orderPage= new OrderPage(driver);
+        loginPage.logIn("testautomation1", "automation123!");
+        dashBoardPage.setOrderLink();
 
     }
-    @Test(priority = 1, dataProvider = "loginData")
-    public void Login(String userName, String password) {
-        loginPage.loginAndLogOut(userName, password);
-        Assert.assertTrue(loginPage.veriFyCubeCartLogo());
+    @Test
+    public void orderListVerify(){
+        orderPage.verifyOrder();
+
+
+    }
+    @Test
+    public void verifyCheckUncheckLick(){
+        orderPage.verifyCheckUncheckAllClick();
+
     }
 
-    @DataProvider
-    public Object[][] loginData() {
-        Object[][] credential = new Object[][]{
-                {"testautomation1", "automation123!"},
 
-        };
-        return credential;
+
+
+
+    @AfterClass
+    public void tearDown(){
+        closeBrowser();
     }
+
+
+
+
+
 }
