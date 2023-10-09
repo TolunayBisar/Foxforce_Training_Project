7c43b3cb37e7b1d7cf15d30cd39f00bce19aa9b3
sherzat1 <alimardan25255@gmail.com>
2023-10-09 12:24:18 +0300

    sprint 2.0

---
+++ src/test/java/inventory/CategoriesPage.java
@@ -0,0 +1,14 @@
+package inventory;
+
+/**@Description all method which I use:
+ * addParentCategory(String categoryName)
+ * addSubCategoryToParentCategory(String subCategoryName, String parentCategoryName)
+ * listOfProductsOfThisCategory(String categoryName)
+ * editCategory(String categoryName)
+ * deleteCategory(int itemId)
+ * @author sherzat
+ * @date   2023/10/9
+ */
+
+public class CategoriesPage {
+}
