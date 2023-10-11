a01d8c0d35046191db67257d7ad5d19a51ac4ce5
sherzat1 <alimardan25255@gmail.com>
2023-10-11 07:31:33 +0300

    sprint 2.2

--- src/test/java/inventory/CategoriesPage.java
+++ src/test/java/inventory/CategoriesPage.java
@@ -1,10 +1,12 @@
 package inventory;
 
 import basefunctions.FunctionLibrary;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 /**@Description all method which I use:
  * addParentCategory(String categoryName)
@@ -27,6 +29,8 @@ public class CategoriesPage {
         PageFactory.initElements(driver,this);
         functionLibrary = new FunctionLibrary(driver);
     }
+
+
     @FindBy(linkText = "Categories")
     WebElement categories;
     @FindBy(linkText = "Add Category")
@@ -36,7 +40,7 @@ public class CategoriesPage {
     @FindBy(xpath = "//input[@name='cat[visible]']/following-sibling::img")
     WebElement visible;
     @FindBy(id = "name")
-    WebElement categoryName;
+    WebElement categoryName1;
     @FindBy(id = "parent")
     WebElement parentCategory;
     @FindBy(linkText = "Description")
@@ -49,6 +53,8 @@ public class CategoriesPage {
     WebElement imageBox;
     @FindBy(id = "fm-icf")
     WebElement createFolder;
+    @FindBy(linkText = "Go")
+    WebElement goButton;
     @FindBy(linkText = "Select File")
     WebElement selectFile;
     @FindBy(linkText = "Search Engines")
@@ -61,6 +67,74 @@ public class CategoriesPage {
     WebElement metaDescription;
     @FindBy(id="cat_save")
     WebElement saveButton;
+    public void addCategory(String categoryName){
+        functionLibrary.waitForElementPresent(categories);
+        categories.click();
+        functionLibrary.waitForElementPresent(addCategory);
+        addCategory.click();
+        functionLibrary.waitForElementPresent(visible);
+        visible.click();
+        functionLibrary.waitForElementPresent(categoryName1);
+        categoryName1.sendKeys(categoryName);
+        Select selectParentCategory = new Select(parentCategory);
+        selectParentCategory.selectByIndex(1);
+        functionLibrary.waitForElementPresent(description);
+        description.click();
+        functionLibrary.waitForElementPresent(input);
+        input.sendKeys();
+        functionLibrary.waitForElementPresent(image);
+        image.click();
+        functionLibrary.waitForElementPresent(selectFile);
+        createFolder.sendKeys("C:\\new.png");
+        functionLibrary.waitForElementPresent(createFolder);
+        createFolder.sendKeys();
+        functionLibrary.waitForElementPresent(goButton);
+        goButton.click();
+        functionLibrary.waitForElementPresent(searchEngines);
+        searchEngines.click();
+        functionLibrary.waitForElementPresent(metaTitle);
+        metaTitle.sendKeys();
+        functionLibrary.waitForElementPresent(seoPath);
+        seoPath.sendKeys();
+        functionLibrary.waitForElementPresent(metaDescription);
+        metaDescription.sendKeys();
+        functionLibrary.waitForElementPresent(saveButton);
+        saveButton.click();
+    }
+    public void addSubCategoryToParentCategory(String subCategoryName, String parentCategoryName){
+        functionLibrary.waitForElementPresent(categories);
+        categories.click();
+        functionLibrary.waitForElementPresent(addCategory);
+        addCategory.click();
+        functionLibrary.waitForElementPresent(visible);
+        visible.click();
+        functionLibrary.waitForElementPresent(categoryName1);
+        categoryName1.sendKeys(subCategoryName);
+        Select selectParentCategory = new Select(parentCategory);
+        selectParentCategory.selectByVisibleText(parentCategoryName);
+        functionLibrary.waitForElementPresent(description);
+        description.click();
+        functionLibrary.waitForElementPresent(input);
+        input.sendKeys();
+        functionLibrary.waitForElementPresent(image);
+        image.click();
+        functionLibrary.waitForElementPresent(selectFile);
+        createFolder.sendKeys("C:\\new.png");
+        functionLibrary.waitForElementPresent(createFolder);
+        createFolder.sendKeys();
+        functionLibrary.waitForElementPresent(goButton);
+        goButton.click();
+        functionLibrary.waitForElementPresent(searchEngines);
+        searchEngines.click();
+        functionLibrary.waitForElementPresent(metaTitle);
+        metaTitle.sendKeys();
+        functionLibrary.waitForElementPresent(seoPath);
+        seoPath.sendKeys();
+        functionLibrary.waitForElementPresent(metaDescription);
+        metaDescription.sendKeys();
+        functionLibrary.waitForElementPresent(saveButton);
+        saveButton.click();
+    }
 
 
 }
