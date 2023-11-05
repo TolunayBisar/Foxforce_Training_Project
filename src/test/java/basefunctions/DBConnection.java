a2f0358a9f2b99c52bcbc52404416cdf37fbc3d3
anargul369 <abdanna369@gmail.com>
2023-11-05 17:54:49 +0100

    Anar1105

---
+++ src/test/java/basefunctions/DBConnection.java
@@ -0,0 +1,90 @@
+package basefunctions;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author : Anar
+ * @created : 2023-10-16,23:09
+ * @Email : abdanna369@gmail.com
+ **/
+
+public class DBConnection {
+    // creat a method to connect to data base
+    public Connection connectToDataBaseServer(
+            String dbUrl,
+            String dbPort,
+            String username,
+            String password,
+            String defaultDB,
+            ConnectionType connectionType) {
+        String JTDS_Driver = "net.sourceforge.jtds.jdbc.Driver"; //MSSQL
+        String MYSQL_Driver = "com.mysql.cj.jdbc.Driver"; //MYSQL
+        Connection connection = null;
+        switch (connectionType) {
+            case MSSQL:
+                try {
+                    Class.forName(JTDS_Driver); //load the driver
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+                String msSqlConnectionUrl = "jdbc:jtds:sqlserver://" + dbUrl + ":" + dbPort + ";DataBaseName=" + defaultDB;
+                try {
+                    connection = DriverManager.getConnection(msSqlConnectionUrl, username, password);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    if (!connection.isClosed()) {
+                        System.out.println("Connection is established!!!");
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case MYSQL:
+                try {
+                    Class.forName(MYSQL_Driver);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+                String mySqlConnectionUrl = "jdbc:mysql://" + dbUrl + ":" + dbPort + "/" + defaultDB + "?useSSL=FALSE";
+                try {
+                    connection = DriverManager.getConnection(mySqlConnectionUrl, username, password);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    if (!connection.isClosed()) {
+                        System.out.println("Connection is established!!!");
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            default:
+                System.out.println("You need to specify a Database Connection type MSSQL or MYSQL!!!");
+                try {
+                    if (!connection.isClosed()) {
+                        System.out.println("Connection is established!!!");
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+        }
+        return connection;
+    }
+
+    public void closeDataBaseConnection(Connection connection) {
+        try {
+            if (connection.isClosed()) {
+                System.out.println("Connection is already closed!");
+            } else {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/inventory/test.java
+++ src/test/java/inventory/test.java
@@ -1,18 +1,30 @@
 package inventory;
 
 import basefunctions.BaseClass;
+import basefunctions.DBConnection;
+import basefunctions.FunctionLibrary;
 import dashboard.DashBoardPage;
 import dashboard.LoginPage;
-import mailinglist.MailingList;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.sql.Connection;
+
 public class test extends BaseClass {
+
     LoginPage loginPage;
     DashBoardPage dashBoardPag;
     ReviewPage reviewPage;
-
-    MailingList mailingList;
+    FunctionLibrary functionLibrary;
+    DBConnection dbConnection;
+    Connection connection;
+//    String fileName="config.properties";
+//    String url = functionLibrary.readFromConfig(fileName,"dburl");
+//    String port = functionLibrary.readFromConfig(fileName,"dbport");
+//    String username = functionLibrary.readFromConfig(fileName,"dbusername");
+//    String password = functionLibrary.readFromConfig(fileName,"dbpassword");
+//    String defaultDB = functionLibrary.readFromConfig(fileName,"dbname");
 
     @BeforeClass
     public void setUp() {
@@ -21,10 +33,29 @@ public class test extends BaseClass {
         loginPage.logIn("testautomation1", "automation123!");
         dashBoardPag = new DashBoardPage(driver);
         reviewPage = new ReviewPage(driver);
+//        dbConnection = new DBConnection();
+//        connection = dbConnection.connectToDataBaseServer(url,port,username,password,defaultDB, ConnectionType.MYSQL);
     }
-    @Test
-    public void ProductReview(){
+    @Test (priority = 1)
+    public void addReview() {
         dashBoardPag.clickOnReviewLink();
-        //reviewPage.addReview("c#");
+        reviewPage.addReview();
+    }
+    @Test(priority = 2)
+    public void editReview(){
+        dashBoardPag.clickOnReviewLink();
+        reviewPage.editProductReview();
+    }
+    @Test(priority = 3)
+    public void deleteReview(){
+        dashBoardPag.clickOnReviewLink();
+        reviewPage.deleteProductReview();
+        Assert.assertTrue(reviewPage.verifyDeleteMessage());
+    }
+    @Test(priority = 4)
+    public void searchReview(){
+        dashBoardPag.clickOnReviewLink();
+        reviewPage.searchOfReview();
+        Assert.assertTrue(reviewPage.verifySearchReview());
     }
 }
