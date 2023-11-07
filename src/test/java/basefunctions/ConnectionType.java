657870ae99a4d8faf0345eaafbd87cf018244783
AlimjanHamut <a.hamut@hotmail.com>
2023-11-07 20:28:39 +0100

    07112023

---
+++ src/test/java/basefunctions/ConnectionType.java
@@ -0,0 +1,10 @@
+package basefunctions;
+
+/**
+ * @author : Alim Hamut
+ * @created : 2023-10-20,7:27 p.m.
+ * @Email : alimhamut.job@gmail.com
+ **/
+public enum ConnectionType {
+    MSSQL, MYSQL
+}
--- src/test/java/dashboard/DashBoardPage.java
+++ src/test/java/dashboard/DashBoardPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DashBoardPage {
-   public WebDriver driver;
+    WebDriver driver;
     FunctionLibrary functionLibrary;
     @FindBy(xpath = "//a[@href=\"#dashboard\"]")
     WebElement dashBoardLink;
@@ -22,10 +22,23 @@ public class DashBoardPage {
     WebElement newsLettersLink;
     @FindBy(linkText = "Mailing List")
     WebElement mailingListLink;
+    @FindBy(linkText = "Import Catalogue")
+    WebElement importCatalogLink;
+    @FindBy(linkText = "Export Catalogue")
+    WebElement exportCatalogLink;
     @FindBy(xpath = "//a[text()=\"Email Log\"]")
     WebElement emailLogLink;
-    @FindBy(css = "a#nav_products")
-    WebElement productsLink;
+
+
+    @FindBy(xpath = "//ul[@id='menu_Inventory']/li/a[text()='Product Options']")
+    WebElement productOptionLink;
+
+    @FindBy(xpath = "//a[text()='Manufacturers']")
+    WebElement manufactureLink;
+
+
+    @FindBy(xpath = "//*[@id=\"menu_Inventory\"]/li[5]/a")
+    WebElement promotionalCodes;
 
 
     public DashBoardPage(WebDriver driver) {
@@ -70,17 +83,41 @@ public class DashBoardPage {
         functionLibrary.waitForElementPresent(mailingListLink);
         mailingListLink.click();
     }
-    public void clickOnEmailLogLink(){
+    public void clickOnImportCatalog(){
+        functionLibrary.waitForElementPresent(importCatalogLink);
+        importCatalogLink.click();
+    }
+    public void clickOnExportCatalog(){
+        functionLibrary.waitForElementPresent(exportCatalogLink);
+        exportCatalogLink.click();
+    }
+
+    public void clickOnEmailLogLink() {
         functionLibrary.waitForElementPresent(emailLogLink);
         emailLogLink.click();
     }
-    public void clickOnProductsLink(){
-        functionLibrary.waitForElementPresent(productsLink);
-        productsLink.click();
+
+
+    public void clickOnProductOptionLink(){
+        functionLibrary.waitForElementPresent(productOptionLink);
+        productOptionLink.click();
     }
 
-    public void logout() {
 
+    public void clickOnManufactureLink() {
+        functionLibrary.waitForElementPresent(manufactureLink);
+        manufactureLink.click();
+    }
+
+
+
+    public void clickOnPromotionalCodes() {
+        functionLibrary.waitForElementPresent(promotionalCodes);
+        promotionalCodes.click();
+    }
+
+
+    public void logout() {
         driver.findElement(By.cssSelector(".fa.fa-sign-out")).click();
     }
 }
